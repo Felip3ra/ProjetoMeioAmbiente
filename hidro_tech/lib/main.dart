513fa1108c887a55f6796e7726a7d3073c331be7
F3LIPERA <felipesantanasantos1812@gmail.com>
2024-08-28 19:14:27 -0300

    feat: tela de esqueceu a senha

--- hidro_tech/lib/main.dart
+++ hidro_tech/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hidro_tech/pages/loginPage.dart';
 import 'package:hidro_tech/pages/signUpPage.dart';
+import 'package:hidro_tech/pages/passwordPage.dart';
 void main(){
   runApp(MyApp());
 }
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: signUpPage(),
+      home: passwordPage(),
     );
   }
 }
