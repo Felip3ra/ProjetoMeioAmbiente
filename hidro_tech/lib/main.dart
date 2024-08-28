ddc0fe669b2f68094d70e3c53ab0772d9d82fc46
F3LIPERA <felipesantanasantos1812@gmail.com>
2024-08-28 17:53:33 -0300

    feat: tela de cadastro

--- hidro_tech/lib/main.dart
+++ hidro_tech/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hidro_tech/pages/loginPage.dart';
+import 'package:hidro_tech/pages/signUpPage.dart';
 void main(){
   runApp(MyApp());
 }
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: loginPage(),
+      home: signUpPage(),
     );
   }
 }
