07784a5e0c80fb83a318f8d26e5aa3bb404432ce
F3LIPERA <felipesantanasantos1812@gmail.com>
2024-08-28 13:34:59 -0300

    feat: adicionando logo

---
+++ hidro_tech/lib/pages/loginPage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class loginPage extends StatefulWidget {
+  const loginPage({super.key});
+
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
+
+class _loginPageState extends State<loginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/logo1.png',width: 157,height: 156.47,),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
