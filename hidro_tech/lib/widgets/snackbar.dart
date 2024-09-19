22e42764e09c3603cad44ca4f86dd00aae59f0a6
Felipe <felipesantanasantos1812@gmail.com>
2024-09-19 00:31:54 -0300

    feat: notificacao de cadastro

--- hidro_tech/lib/widgets/snackbar.dart
+++ hidro_tech/lib/widgets/snackbar.dart
@@ -7,7 +7,10 @@ showSnackBar(
   SnackBar snackbar = SnackBar(
     content: Text(texto),
     backgroundColor: (isErro)? Colors.red:Colors.green,
-    
+    duration: Duration(seconds: 4),
+    action: SnackBarAction(label: "OK", textColor: Colors.white ,onPressed: (){
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    }),
   );
   ScaffoldMessenger.of(context).showSnackBar(snackbar);
 }
